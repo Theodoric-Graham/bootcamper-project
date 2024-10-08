12becfbf48508b76884230cba0f53f0ff1c757a1
d-t.graham <d-t.graham@AFSWW1NKZ7CP3L.us.accenturefederal.com>
2024-10-08 17:24:21 -0500

    Organized classes but broke the games display

--- mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -1,64 +1,45 @@
 package com.organization.mvcproject.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.organization.mvcproject.model.Game;
+import com.organization.mvcproject.model.GameImpl;
+import com.organization.mvcproject.api.service.GameService;
+import com.organization.mvcproject.repository.MockGameDAO;
 
 @Service
 public class GameServiceImpl implements GameService {
 
-	/**
-	 * TODO 2.0 The class that interacts with persistent data is called a Data Access Object(DAO)
-	 *  or a Repository class. The private static list is mocking our persistance of games.
-	 *   
-	 *  Move this list, and methods operating on this list to an appropriately named package and class.
-	 */
+	@Autowired
+	private MockGameDAO gameDao;
 	
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
-
-	static {
-		games = populateGames();
+	@Override
+	public List<GameImpl> retrieveAllGames() {
+		return gameDao.retrieveAllGames();
+	}
+	
+	@Override
+	public GameImpl saveGame(GameImpl game) {
+		return gameDao.saveOrUpdateGame(game);
+	}
+	
+	public GameImpl updateGame(GameImpl game) {
+		return gameDao.saveOrUpdateGame(game);
 	}
 
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
+	public boolean deleteGameById(Long gameId) {
+		return gameDao.deleteGameById(gameId);
 	}
 
 	@Override
-	public List<Game> retrieveAllGames() {
-		return games;
+	public List<GameImpl> findGamesByGenre(String genre) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	@Override
-	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
-		return game;
-	}
+
+
 
 }
